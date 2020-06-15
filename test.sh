3346191d2843b30986f22ef9d0e1afb89f9ce63b
Shree <5095331+Shreeshrii@users.noreply.github.com>
2020-06-15 12:16:25 +0000

    Update  test data

--- test.sh
+++ test.sh
@@ -3,8 +3,9 @@
 lang=RajaRajanOne
 PSM=6
 cd test/
+rm  -v *ALL*$lang*.txt  *tessinput.tif
 my_files=$(ls *{*.jpg,*.tif,*.png,*.jp2})
-    rm  -v *ALL*$lang*.txt  *tessinput.tif
+
     for my_file in ${my_files}; do
             echo -e "\n ***** "  $my_file "LANG" $lang  $PSM "****"
             OMP_THREAD_LIMIT=1 time -p tesseract $my_file  "${my_file%.*}-$lang-tmp" --oem 1 --psm $PSM -l "$lang"  --dpi 300 --tessdata-dir /home/ubuntu/tess5training-rajarajan/tessdata/best -c preserve_interword_spaces=1 -c page_separator='' -c tessedit_write_images=true
